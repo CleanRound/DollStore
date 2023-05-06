e2624ff138447fb122189596b9eaac63ede2aeaf
Oleg Lutsik <oleglutsik.dev@gmail.com>
2023-05-06 11:01:27 +0300

    Update: Upload Task-001 files

---
+++ application/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "doll.h"
+
+int main() {
+    Doll barbie;
+    create_doll(barbie, "Barbie", 10.99, 100);
+    barbie.print_doll();
+    barbie.update_doll("New Barbie", 15.99, 75);
+    barbie.print_doll();
+    return 0;
+}
